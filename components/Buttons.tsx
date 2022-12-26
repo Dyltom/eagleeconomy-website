68f621a74766eb5bf456526e52364167531537e1
dyltom <dylanthenderson@yahoo.com.au>
2022-12-26 16:04:32 +1100

    initial commit

---
+++ components/Buttons.tsx
@@ -0,0 +1,21 @@
+import { Button, ButtonProps } from "@mantine/core";
+import { DiscordIcon } from "@mantine/ds";
+
+export function DiscordButton(props: ButtonProps) {
+    return (
+        <Button
+            leftIcon={<DiscordIcon size={16} />}
+            sx={(theme) => ({
+                backgroundColor:
+                    theme.colorScheme === "dark" ? "#5865F2" : "#5865F2",
+                "&:hover": {
+                    backgroundColor:
+                        theme.colorScheme === "dark"
+                            ? theme.fn.lighten("#5865F2", 0.05)
+                            : theme.fn.darken("#5865F2", 0.05),
+                },
+            })}
+            {...props}
+        />
+    );
+}
